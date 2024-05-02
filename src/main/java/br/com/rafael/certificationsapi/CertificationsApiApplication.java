a8ff13b909783b6150d77862b9b70b3d6c7a29f4
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-05-01 22:29:49 -0300

    first commit

---
+++ src/main/java/br/com/rafael/certificationsapi/CertificationsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafael.certificationsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificationsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CertificationsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/rafael/certificationsapi/CertificationsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafael.certificationsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
